2cfb9c6a85a5dd51ef2990ef8058727fa2ebeac4
Oliver Bucher <level8broccoli@gmail.com>
2020-07-11 12:14:33 +0200

    give action names

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,10 +3,29 @@ import Logger from './Logger.js';
 
 export const initResources = (res, n) => {
   Logger.log('filled bag randomly', n);
-  let icons = ['forward-sun', 'minerals', 'nautilus-shell', 'ninja-star', 'ore', 'dragon-head'];
+  let random = [{
+    name: 'Push',
+    file: 'forward-sun'
+  }, {
+    name: 'Cristal',
+    file: 'minerals'
+  }, {
+    name: 'Shell',
+    file: 'nautilus-shell'
+  }, {
+    name: 'Ninjas',
+    file: 'ninja-star'
+  }, {
+    name: 'Stone',
+    file: 'ore'
+  }, {
+    name: 'Dragon',
+    file: 'dragon-head'
+  }];
   while (n > 0) {
-    const randomIndex = Math.floor(Math.random() * icons.length);
-    const action = new Action(n, `Action ${n--}`, icons[randomIndex], res);
+    const randomIndex = Math.floor(Math.random() * random.length);
+    const randomObj = random[randomIndex];
+    const action = new Action(n--, randomObj.name, randomObj.file, res);
     res.upcoming.push(action);
   }
 };
