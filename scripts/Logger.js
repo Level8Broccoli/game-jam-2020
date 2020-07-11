1937e47f98b8161aad126d1bf91ca70d2a47d4c6
Oliver Bucher <level8broccoli@gmail.com>
2020-07-11 12:18:16 +0200

    change output

--- scripts/Logger.js
+++ scripts/Logger.js
@@ -2,7 +2,7 @@ export default class Logger {
   static log(...args) {
     console.trace(...args);
     const pre = document.createElement('pre');
-    const text = document.createTextNode(...args);
+    const text = document.createTextNode(args.join(' '));
     pre.append(text);
     document.querySelector('.log').append(pre);
   }
